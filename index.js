96c3a5e25623328338b1a232cc36783be37dc00c
Mohit Bajaj <100280642+MohitBajaj33@users.noreply.github.com>
2023-10-07 00:03:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  const value = document.querySelector("input").value;
+  const mainDiv = document.querySelector(".contan_movie");
+
+
+   
+
+
+  const url =
+    `https://online-movie-database.p.rapidapi.com/auto-complete?q=${value}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "dc28c9d449mshf55de20098e3570p1e7e97jsncdf28147a381",
+      "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+    },
+  };
+  fetch(url, options)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+        const n=data.d.length;
+        setTimeout(()=>{
+            for(let k=0;k<n;k++){
+                const createNewDiv = document.createElement('div');
+                const image = document.createElement('img');
+                createNewDiv.appendChild(image);
+                const newDiv= mainDiv.appendChild(createNewDiv).classList.add("movie1");
+                image.src=data.d[k].i.imageUrl;
+                console.log(n);
+            }
+        },2000);
+
+      
+    });
+
+    
+});
